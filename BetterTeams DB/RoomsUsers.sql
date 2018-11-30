accb788997a46b43e8abbe90a8f2fa956ae43e86
Maria Marousopoulou <marousopoulou@gmail.com>
2018-11-30 12:37:45 +0200

    DB Scripts v 2.0.0

---
+++ BetterTeams DB/RoomsUsers.sql
@@ -0,0 +1,35 @@
+USE [BetterTeams]
+GO
+
+/****** Object:  Table [dbo].[RoomsUsers]    Script Date: 30/11/2018 12:32:22 μμ ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[RoomsUsers](
+	[RoomId] [int] NOT NULL,
+	[Email] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_RoomsUsers] PRIMARY KEY CLUSTERED 
+(
+	[RoomId] ASC,
+	[Email] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[RoomsUsers]  WITH CHECK ADD  CONSTRAINT [FK_RoomsUsers_Rooms] FOREIGN KEY([RoomId])
+REFERENCES [dbo].[Rooms] ([Id])
+GO
+
+ALTER TABLE [dbo].[RoomsUsers] CHECK CONSTRAINT [FK_RoomsUsers_Rooms]
+GO
+
+ALTER TABLE [dbo].[RoomsUsers]  WITH CHECK ADD  CONSTRAINT [FK_RoomsUsers_Users] FOREIGN KEY([Email])
+REFERENCES [dbo].[Users] ([Email])
+GO
+
+ALTER TABLE [dbo].[RoomsUsers] CHECK CONSTRAINT [FK_RoomsUsers_Users]
+GO
+
